71b526a8bede2850ccdac8fa4e7665168622bb08
Sergey Anufrienko <serg@anufrienko.net>
2016-08-24 11:56:56 +0300

    Add files via upload

---
+++ DebianPackageListParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Dynamic;
+
+namespace MadProgrammer
+{
+    public class DebianPackageListParser
+    {
+        public static IEnumerable<dynamic> Parse(string filename)
+        {
+            return Parse(new StreamReader(new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite), true));
+        }
+
+        public static IEnumerable<dynamic> Parse(TextReader reader)
+        {
+            string line;
+            var item = new ExpandoObject() as IDictionary<string, object>;
+
+            while ((line = reader.ReadLine()) != null) {
+                if (line != "")
+                {
+                    // Sanity check
+                    if (line.StartsWith(" ") || line.IndexOf(':') == -1)
+                        continue;
+
+                    var split = line.Split(new char[] { ':' }, 2);
+                    item.Add(split[0], split[1].Trim());
+                }
+                else
+                {
+                    yield return item;
+                    item = new ExpandoObject() as IDictionary<string, object>;
+                }
+            }
+        }
+    }
+}
